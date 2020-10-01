9f6f6f41855f7ed01997decf80da4e13fe294336
John Talbot <johntalbot@jtalbot.home>
2020-10-01 08:23:15 -0400

    Initial endpoints for web-app

--- web-app/app.py
+++ web-app/app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask import render_template
+from flask import make_response
+from flask import request
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def serve_index():
+    return render_template('index.html')
+
+@app.route('/create-flight', methods=["POST"])
+def create_flight():
+    print("create flight")
+    print(request.form)
+    return make_response("ok", 200)
+
+@app.route('/create-flight')
+def create_airport():
+    print("create airport")
+    print(request.form)
+    return make_response("ok", 200)
